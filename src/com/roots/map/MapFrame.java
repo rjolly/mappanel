3d4f28d45f99c5bb54d0d23be9f642e1c9fe5d9b
Raphael Jolly <rjolly@users.sourceforge.net>
2018-02-08 17:41:58 +0100

    empty

--- src/com/roots/map/MapFrame.java
+++ src/com/roots/map/MapFrame.java
@@ -1,6 +1,9 @@
 package com.roots.map;
 
 import java.awt.BorderLayout;
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.net.URI;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JMenuBar;
@@ -22,15 +25,32 @@ public class MapFrame extends Frame {
 		setScheme("geo");
 	}
 
+	private void setLocation(final double lon, final double lat) {
+		final MapPanel panel = gui.getMapPanel();
+		panel.setZoom(8);
+		final Point position = panel.computePosition(new Point2D.Double(lon, lat));
+		panel.setCenterPosition(position);
+		panel.repaint();
+	}
+
 	@Override
 	public void open() {
-		gui = new MapPanel.Gui();
-		getContentPane().add(gui, BorderLayout.CENTER);
-		setJMenuBar(gui.createMenuBar());
+		if (gui == null) {
+			gui = new MapPanel.Gui();
+			getContentPane().add(gui, BorderLayout.CENTER);
+			setJMenuBar(gui.createMenuBar());
+		}
+		final URI uri = getURI();
+		if (uri != null) {
+			final String ssp = uri.getSchemeSpecificPart();
+			final String s[] = ssp.split(",");
+			setLocation(Double.parseDouble(s[0]), Double.parseDouble(s[1]));
+		}
 	}
 
 	@Override
 	public void close() {
+		setURI(null);
 		getContentPane().remove(gui);
 		gui = null;
 	}
