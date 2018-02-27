2b836c668ad8e325c94f4b498e4f828ed7da0146
Raphael Jolly <rjolly@users.sourceforge.net>
2018-02-27 17:37:33 +0100

    empty

---
+++ src/com/roots/map/OptionPanel.java
@@ -0,0 +1,44 @@
+package com.roots.map;
+
+public class OptionPanel extends linoleum.application.OptionPanel {
+
+	public OptionPanel() {
+		initComponents();
+	}
+
+	@SuppressWarnings("unchecked")
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
+
+                jLabel1 = new javax.swing.JLabel();
+                jTextField1 = new javax.swing.JTextField();
+
+                jLabel1.setText("Tile server :");
+
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+                this.setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 319, Short.MAX_VALUE)
+                                .addContainerGap())
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLabel1)
+                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                );
+        }// </editor-fold>//GEN-END:initComponents
+
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JLabel jLabel1;
+        private javax.swing.JTextField jTextField1;
+        // End of variables declaration//GEN-END:variables
+}
